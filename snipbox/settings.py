2b360e8b539e652eeab068fdc299104aa83d4f2f
meghapp3011 <meghapp3011@gmail.com>
2025-10-30 11:01:45 +0530

    flake8 added for correcting coding standard

--- snipbox/settings.py
+++ snipbox/settings.py
@@ -126,8 +126,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
